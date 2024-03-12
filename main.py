01c5155538082d44074257f935828c31e8798ebf
Viktoria <viky.ne2@gmail.com>
2024-03-13 00:15:41 +0300

    Ввод с клавиатуры

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+# list_1 = ["Hello", "2", "world", ":-)"]
+list_2=[]
+print("Введите массив: ")
+list_1=input().split(',')
+
+for i in range(random.randint(0,3)):
+    rand=random.choice(list_1)
+    if rand not in list_2:
+        list_2.append(rand)
+print (list_2)
