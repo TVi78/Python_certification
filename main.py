4913270b8cfedfee3d7784aaf265c00ea4e7f8fc
Viktoria <viky.ne2@gmail.com>
2024-03-13 00:16:43 +0300

    Заданный первый массив

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random
 
-# list_1 = ["Hello", "2", "world", ":-)"]
+list_1 = ["Hello", "2", "world", ":-)"]
 list_2=[]
-print("Введите массив: ")
-list_1=input().split(',')
+# print("Введите массив: ")
+# list_1=input().split(',')
 
 for i in range(random.randint(0,3)):
     rand=random.choice(list_1)
